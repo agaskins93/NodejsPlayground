ddf80678396c850e999c517fbd2390486fe7ef4e
agaskins93 <AveryGaskins93@gmail.com>
2019-12-04 21:04:48 -0500

    still aligning image

--- ngworkspace/src/app/warningmsg/warningmsg.component.ts
+++ ngworkspace/src/app/warningmsg/warningmsg.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
  selector: 'warn-msg',
-templateUrl: './warningmsg.component.html' 
+templateUrl: './warningmsg.component.html', 
+styleUrls: ['./warningmsg.component.css']
 })
 export class WarningMsg {
   
