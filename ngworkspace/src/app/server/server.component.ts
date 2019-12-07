96c5bdf242d0852bcd4676c514cf41e9593a5c29
agaskins93 <AveryGaskins93@gmail.com>
2019-12-07 14:58:06 -0500

    adding string interpolation in server comp

--- ngworkspace/src/app/server/server.component.ts
+++ ngworkspace/src/app/server/server.component.ts
@@ -4,5 +4,12 @@ selector: 'app-server',
 templateUrl: './server.component.html'
 })
 export class ServerComponent {
-  
+  serverId: number = 14;
+  serverStatus: string = 'online';
+  name: string = 'ASDFOKDSF';
+  getMessage: string = 'Please turn off this server'
+
+  returnStatusMessage() {
+    return this.getMessage;
+  }
 }
